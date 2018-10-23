af07c95e8e8adf7b43e6fbe3f113ca60086fe07f
Andrea Sponziello <andrea.sponziello@gmail.com>
2018-10-23 12:17:29 +0200

    port 80

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ app.post('/login', function (req, res) {
 app.get('/', function (req, res) {
   res.send('hello world')
 });
-
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+var port = 80;
+app.listen(port, function () {
+    console.log('Example app listening on port ', port);
 });
 
 
