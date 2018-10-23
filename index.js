5418870de84ebddcc0548c70e2a9c74e761f1f25
Andrea Sponziello <andrea.sponziello@gmail.com>
2018-10-23 12:20:13 +0200

    port3000

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.post('/login', function (req, res) {
 app.get('/', function (req, res) {
   res.send('hello world')
 });
-var port = 80;
+var port = 3000;
 app.listen(port, function () {
     console.log('Example app listening on port ', port);
 });
